3462536d74432fe2ee9e01d6b62308224d683d8b
terranc <wuchaoboy@gmail.com>
2016-07-14 00:18:20 +0800

    漏了toptips.vue

---
+++ src/components/vux_extension/toptips.vue
@@ -0,0 +1,59 @@
+<template>
+  <div v-if="show" class="weui_toptips" :transition="transition" :class="toggleClass">
+    <slot></slot>
+  </div>
+</template>
+
+<style type="scss">
+.weui_toptips{
+  display: block;
+  transform: translateY(0%);
+}
+.vux-slide-down-transition{
+  transition: transform linear 0.2s
+}
+.vux-slide-down-enter, .vux-slide-down-leave{
+  transform: translateY(-100%);
+}
+</style>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    time: {
+      type: Number,
+      default: 3000,
+    },
+    type: {
+      type: String,
+      default: 'warn',
+    },
+    transition: {
+      type: String,
+      default: 'vux-slide-down',
+    },
+  },
+  computed: {
+    toggleClass() {
+      return {
+        weui_warn: this.type === 'warn',
+        weui_success: this.type === 'success',
+      };
+    },
+  },
+  watch: {
+    show(val) {
+      if (val) {
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          this.show = false;
+        }, this.time);
+      }
+    },
+  },
+};
+</script>
