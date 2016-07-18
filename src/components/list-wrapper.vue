8e63afdb4dd8817cec4abe28559249acf4804ab1
韦捷 <weijiewow@163.com>
2016-07-19 03:07:14 +0800

    初始化列表组件

---
+++ src/components/list-wrapper.vue
@@ -0,0 +1,36 @@
+<template>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    more: {
+      type: Boolean,
+      default: false,
+    },
+    autoLoad: {
+      type: Boolean,
+      default: false,
+    },
+    api: {
+      type: String,
+    },
+    params: {
+      type: String,
+    },
+    query: {
+      type: String,
+    },
+  },
+};
+</script>
+
