96a7a4583d2c051889e76c6ac3ebed2b3cccaf0b
韦捷 <weijiewow@163.com>
2016-07-21 01:14:17 +0800

    将状态页的组件逻辑和页面逻辑分离

--- src/pages/state/timeout.vue
+++ src/pages/state/timeout.vue
@@ -1,17 +1,8 @@
 <template>
   <vue-helmet :title='title' v-ref:head></vue-helmet>
-  <div class="weui_msg">
-    <div class="weui_icon_area"><i class="weui_icon_warn weui_icon_msg"></i></div>
-    <div class="weui_text_area">
-      <h2 class="weui_msg_title">{{ message }}</h2>
-      <p class="weui_msg_desc">{{ detail }}</p>
-    </div>
-    <div class="weui_opr_area">
-      <p class="weui_btn_area">
-        <a class="weui_btn weui_btn_primary" @click="reload">重新加载</a>
-      </p>
-    </div>
-  </div>
+  <status :message="message" :text="text">
+    <a class="weui_btn weui_btn_primary" @click="reload" slot="buttons">重新加载</a>
+  </status>
 </template>
 
 <style lang="scss">
@@ -19,21 +10,19 @@
 
 <script>
 import VueHelmet from 'vue-helmet';
-import XButton from 'vux-components/x-button';
-import Tip from 'vux-components/tip';
+import Status from 'components/status';
 
 export default {
   data() {
     return {
       title: '悲剧啦',
       message: '请求超时',
-      detail: '',
+      text: '',
     };
   },
   components: {
     VueHelmet,
-    XButton,
-    Tip,
+    Status,
   },
   methods: {
     reload() {
