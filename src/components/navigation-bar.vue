cc01495951d445917478dcd623dfcc58b742b717
韦捷 <weijiewow@163.com>
2016-08-09 01:54:11 +0800

    加入 MaterialIcons

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -1,19 +1,19 @@
 <template>
   <tabbar id="navigation-bar" v-show="show">
     <tabbar-item v-link="{name: 'index'}" :selected="selected === 'home'">
-      <f-icon slot="icon" name="home" scale="1.6"></f-icon>
+      <i class="material-icons" slot="icon">home</i>
       <span slot="label">首页</span>
     </tabbar-item>
     <tabbar-item v-link="{name: 'articles'}" :selected="selected === 'article'">
-      <f-icon slot="icon" name="columns" scale="1.6"></f-icon>
+      <i class="material-icons" slot="icon">list</i>
       <span slot="label">文章</span>
     </tabbar-item>
     <tabbar-item v-link="{name: 'form'}" :selected="selected === 'form'">
-      <f-icon slot="icon" name="user-plus" scale="1.6"></f-icon>
+      <i class="material-icons" slot="icon">storage</i>
       <span slot="label">表单</span>
     </tabbar-item>
     <tabbar-item v-link="{name: 'setting'}" :selected="selected === 'settings'">
-      <f-icon slot="icon" name="cog" scale="1.6"></f-icon>
+      <i class="material-icons" slot="icon">apps</i>
       <span slot="label">设置</span>
     </tabbar-item>
   </tabbar>
